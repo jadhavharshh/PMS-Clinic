bb10e891cd29ce66638e739bd559f87a53c496ba
Harsh Jadhav <harshjadhavconnect@gmail.com>
2025-04-28 06:46:07 +0000

    Add the Home page and add Loading animation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,44 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Auth from './pages/Auth/Auth'
-import Dashboard from './pages/Dashboard/Dashboard'
-import CreatePatient from './pages/CreatePatient/CreatePatient'
-import ListPatient from './pages/ListPatient/ListPatient'
-import NotFound from './pages/NotFound/NotFound'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Suspense, lazy } from 'react'
+import Home from './pages/Home/Home'
+import { ScaleLoader } from 'react-spinners'
+
+
+// Lazy-load pages
+const Auth = lazy(() => import('./pages/Auth/Auth'))
+const Dashboard = lazy(() => import('./pages/Dashboard/Dashboard'))
+const CreatePatient = lazy(() => import('./pages/CreatePatient/CreatePatient'))
+const ListPatient = lazy(() => import('./pages/ListPatient/ListPatient'))
+const NotFound = lazy(() => import('./pages/NotFound/NotFound'))
+
+function LoadingFallback() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <ScaleLoader 
+        color="#4A90E2" 
+        height={50}
+        width={5}
+        radius={2}
+        margin={2}
+      />
+    </div>
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/create-patient" element={<CreatePatient />} />
-        <Route path="/list-patient" element={<ListPatient />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<LoadingFallback />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/create-patient" element={<CreatePatient />} />
+          <Route path="/list-patient" element={<ListPatient />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
 }
