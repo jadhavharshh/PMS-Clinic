bedc98ae7cb71b5bd0bbf0ec65e18019e678156b
jadhavharshh <harshjadhavconnect@gmail.com>
2025-04-28 09:04:14 +0530

    Setup Dashbaord page

--- client/src/pages/Auth/Auth.tsx
+++ client/src/pages/Auth/Auth.tsx
@@ -1,10 +1,8 @@
+import React from 'react'
 
 const Auth = () => {
   return (
-    <div>
-      <h1>Authentication Page</h1>
-      <p>Login or register here</p>
-    </div>
+    <div>Auth</div>
   )
 }
 
